5c2ff854a655bbc40132edd01cfe55f2e663e55e
Axel GAND <axel.gand5@etu.univ-lorraine.fr>
2020-03-03 17:42:51 +0100

    suppression node_module + modif server.js commands/:id

--- commande_dev/server.js
+++ commande_dev/server.js
@@ -10,107 +10,169 @@ const HOST = "0.0.0.0";
 
 const app = express();
 
-app.use(bodyparser.urlencoded({ extended:false}));
+app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 //GET
 
 app.get("/", (req, res) => {
-  res.send("Commande API\n");
+    res.send("Commande API\n");
 });
 
 app.get('/commands', function (req, res) {
-  let status = req.param('s');
-  let page = req.param('page');
-  let size = req.param('size');
+    let status = req.param('s');
+    let page = req.param('page');
+    let size = req.param('size');
 
-  let queryCommandes = null;
-  let countCommandes = null;
+    let queryCommandes = null;
+    let countCommandes = null;
 
-  if(typeof page === 'undefined' || page <= 0) page = 1;
-  if(typeof size === 'undefined') size = 10;
+    if (typeof page === 'undefined' || page <= 0) page = 1;
+    if (typeof size === 'undefined') size = 10;
 
-  if(typeof status !== 'undefined') countCommandes = `SELECT * FROM commande where status = '${status}'`;
-  else countCommandes = `SELECT * FROM commande`;
+    if (typeof status !== 'undefined') countCommandes = `SELECT * FROM commande where status = '${status}'`;
+    else countCommandes = `SELECT * FROM commande`;
 
-  let count = 0;
-  db.query(countCommandes, (err, result) => {
-      if (err) console.error(err);
-      else {
-        count = result.length;
+    let count = 0;
+    db.query(countCommandes, (err, result) => {
+        if (err) console.error(err);
+        else {
+            count = result.length;
 
-        let pageMax = Math.ceil(count/size);
-        if(page > pageMax) page = pageMax;
-        let debutLimit = (page-1)*size;
+            let pageMax = Math.ceil(count / size);
+            if (page > pageMax) page = pageMax;
+            let debutLimit = (page - 1) * size;
 
-        if(typeof status !== 'undefined') queryCommandes = `SELECT id, nom, created_at, livraison, status FROM commande where status = '${status}' order by livraison ASC, created_at ASC limit ${debutLimit}, ${size}`;
-        else queryCommandes = `SELECT id, nom, created_at, livraison, status FROM commande order by livraison ASC, created_at ASC limit ${debutLimit}, ${size}`;
+            if (typeof status !== 'undefined') queryCommandes = `SELECT id, nom, created_at, livraison, status FROM commande where status = '${status}' order by livraison ASC, created_at ASC limit ${debutLimit}, ${size}`;
+            else queryCommandes = `SELECT id, nom, created_at, livraison, status FROM commande order by livraison ASC, created_at ASC limit ${debutLimit}, ${size}`;
 
-        db.query(queryCommandes, (err, result) => {
-            if (err) console.error(err);
-            else {
-              let next = parseInt(page)+1;
-              if(next > pageMax) next = pageMax;
+            db.query(queryCommandes, (err, result) => {
+                if (err) console.error(err);
+                else {
+                    let next = parseInt(page) + 1;
+                    if (next > pageMax) next = pageMax;
 
-              let prev = page-1;
-              if(prev < 1) prev = 1;
+                    let prev = page - 1;
+                    if (prev < 1) prev = 1;
 
-              result.forEach( function(commande, index) { result[index] = JSON.parse(JSON.stringify({ command: commande, links: {self: {href: "/commands/"+commande.id+"/"}} })); })
-              res.json({
-                  "type": "collection",
-                  "count": count,
-                  "size": size,
-                  "links": {
-                      "next": {
-                          "href": "/commands/?page=" + next +  "&size=" + size
-                      },
-                      "prev": {
-                          "href": "/commands/?page=" + prev + "&size=" + size
-                      },
-                      "last":{
-                          "href": "/commands/?page=" + pageMax + "&size=" + size
-                      },
-                      "first":{
-                          "href": "/commands/?page=1&size=" + size
-                      },
+                    result.forEach(function (commande, index) {
+                        result[index] = JSON.parse(JSON.stringify({
+                            command: commande,
+                            links: {self: {href: "/commands/" + commande.id + "/"}}
+                        }));
+                    })
+                    res.json({
+                        "type": "collection",
+                        "count": count,
+                        "size": size,
+                        "links": {
+                            "next": {
+                                "href": "/commands/?page=" + next + "&size=" + size
+                            },
+                            "prev": {
+                                "href": "/commands/?page=" + prev + "&size=" + size
+                            },
+                            "last": {
+                                "href": "/commands/?page=" + pageMax + "&size=" + size
+                            },
+                            "first": {
+                                "href": "/commands/?page=1&size=" + size
+                            },
 
-                  },
-                  "commands": result
-              })
-            }
-        })
-      }
-  })
+                        },
+                        "commands": result
+                    })
+                }
+            })
+        }
+    })
 })
 
 app.get('/commands/:id', function (req, res) {
-  let queryCommandesId = "SELECT * FROM commande WHERE id = " + "'" + req.params.id + "'";
-  db.query(queryCommandesId, (err, result) => {
-    if (err) {
-      let erreur = {
-        "type": "error",
-        "error": 500,
-        "message": err
-      }
-      JSON.stringify(erreur)
-      res.send(erreur)
-    }
-    else {
-      if (result == "") {
-        let erreur = {
-          "type": "error",
-          "error": 404,
-          "message": req.params.id + " isn't a valid id"
+    let queryCommandesId = "SELECT id,created_at,livraison,nom,mail,montant FROM commande WHERE id = " + "'" + req.params.id + "'";
+    let queryCommandesItems = "SELECT uri,libelle,tarif,quantite FROM item WHERE command_id = " + "'" + req.params.id + "'";
+    db.query(queryCommandesId, (err1, result1) => {
+        if (err1) {
+            let erreur = {
+                "type": "error",
+                "error": 500,
+                "message": err1
+            };
+            JSON.stringify(erreur);
+            res.send(erreur);
+        } else if (result1 === "") {
+            let erreur = {
+                "type": "error",
+                "error": 404,
+                "message": req.params.id + " isn't a valid id"
+            };
+            JSON.stringify(erreur);
+            res.send(erreur);
+        } else db.query(queryCommandesItems, (err2, result2) => {
+            if (err2) {
+                let erreur = {
+                    "type": "error",
+                    "error": 500,
+                    "message": err2
+                };
+                JSON.stringify(erreur);
+                res.send(erreur);
+            } else if (result2 === "") {
+                let erreur = {
+                    "type": "error",
+                    "error": 404,
+                    "message": req.params.id + " isn't a valid id"
+                };
+                JSON.stringify(erreur);
+                res.send(erreur);
+            } else {
+                res.json({
+                    "type": "resource",
+                    "links": {
+                        "self": "/commands/" + req.params.id + "/",
+                        "items": "/commands/" + req.params.id + "/items/"
+                    },
+                    "command": {
+                        "id": result1[0].id,
+                        "created_at": result1[0].created_at,
+                        "livraison": result1[0].livraison,
+                        "nom": result1[0].nom,
+                        "mail": result1[0].mail,
+                        "montant": result1[0].montant,
+                        "items": result2
+                    }
+                })
+            }
+        });
+    });
+});
+
+app.get('/commands/:id/items', function (req, res) {
+    let queryCommandesItems = "SELECT uri,libelle,tarif,quantite FROM item WHERE command_id = " + "'" + req.params.id + "'";
+    db.query(queryCommandesItems, (err, result) => {
+        if (err) {
+            let erreur = {
+                "type": "error",
+                "error": 500,
+                "message": err
+            };
+            JSON.stringify(erreur);
+            res.send(erreur);
+        } else if (result === "") {
+            let erreur = {
+                "type": "error",
+                "error": 404,
+                "message": req.params.id + " isn't a valid id"
+            };
+            JSON.stringify(erreur);
+            res.send(erreur);
+        } else {
+            res.json({
+                "items": result
+            })
         }
-        JSON.stringify(erreur)
-        res.send(erreur)
-      }
-      else {
-        res.send(result)
-      }
-    }
-  });
-})
+    });
+});
 
 app.get("/*", (req, res) => {
     let erreur = {
@@ -126,7 +188,7 @@ app.get("/*", (req, res) => {
 
 app.post("/", (req, res) => {
     let date = new Date()
-    let dateActu = date.getFullYear() + '-' + (date.getMonth()+1)  + '-' + date.getDate() +  ' ' + (date.getHours()+1) + ':'+ date.getMinutes() + ':'+date.getSeconds();
+    let dateActu = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + (date.getHours() + 1) + ':' + date.getMinutes() + ':' + date.getSeconds();
 
     const patternMail = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
     const patternLivraison = /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/i;
@@ -138,33 +200,33 @@ app.post("/", (req, res) => {
     const livraison = req.param('livraison');
     let montant = 3 + (Math.random() * (50 - 3));
 
-    if(typeof email !== 'undefined' && typeof name !== 'undefined' && typeof livraison !== 'undefined') {
-      if(email.match(patternMail)) {
-        if(livraison.match(patternLivraison)) {
-          let query = `Insert into commande (id,created_at,updated_at,livraison,nom,mail,montant) values ('${uid}','${dateActu}','${dateActu}','${livraison}','${name}','${email}',${montant})`;
-          let newCommande = `Select * from commande where id = '${uid}'`;
-          db.query(query, (err, result) => {
-              if (err) console.error(err);
-              else {
-                db.query(newCommande, (err,result) => {
-                  if (err) console.error(err);
-                  else {
-                    res.location('/commandes/' + uid);
-                    res.status(201).json(result);
-                  }
-                })
-              }
-          });
-        } else res.status(400).send('La date et l\'heure de la livraison ne sont pas au bont format : YYYY-MM-DD HH:MM:SS');
-      } else res.status(400).send('L\'email n\'est pas au bont format');
+    if (typeof email !== 'undefined' && typeof name !== 'undefined' && typeof livraison !== 'undefined') {
+        if (email.match(patternMail)) {
+            if (livraison.match(patternLivraison)) {
+                let query = `Insert into commande (id,created_at,updated_at,livraison,nom,mail,montant) values ('${uid}','${dateActu}','${dateActu}','${livraison}','${name}','${email}',${montant})`;
+                let newCommande = `Select * from commande where id = '${uid}'`;
+                db.query(query, (err, result) => {
+                    if (err) console.error(err);
+                    else {
+                        db.query(newCommande, (err, result) => {
+                            if (err) console.error(err);
+                            else {
+                                res.location('/commandes/' + uid);
+                                res.status(201).json(result);
+                            }
+                        })
+                    }
+                });
+            } else res.status(400).send('La date et l\'heure de la livraison ne sont pas au bont format : YYYY-MM-DD HH:MM:SS');
+        } else res.status(400).send('L\'email n\'est pas au bont format');
     } else res.status(400).send('Veuillez entrer un nom, un mail et la date et l\'heure de livraison (nom, mail, livraison)');
 });
 
-app.post("/:id", (req, res) =>{
+app.post("/:id", (req, res) => {
     const id = req.params.id;
 
     let date = new Date()
-    let dateActu = date.getFullYear() + '-' + (date.getMonth()+1)  + '-' + date.getDate() +  ' ' + (date.getHours()+1) + ':'+ date.getMinutes() + ':'+date.getSeconds();
+    let dateActu = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + (date.getHours() + 1) + ':' + date.getMinutes() + ':' + date.getSeconds();
 
     const patternMail = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
     const patternLivraison = /^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/i;
@@ -173,64 +235,64 @@ app.post("/:id", (req, res) =>{
     const email = req.param('mail');
     const livraison = req.param('livraison');
 
-    if(typeof email !== 'undefined' && typeof name !== 'undefined' && typeof livraison !== 'undefined') {
-      if(email.match(patternMail)) {
-        if(livraison.match(patternLivraison)) {
-          let query = `UPDATE commande SET nom = '${name}', mail = '${email}', updated_at = '${dateActu}', livraison = '${livraison}' where id = '${id}'`;
-          let newCommande = `Select * from commande where id = '${id}'`;
-          db.query(query, (err, result) => {
-            if(result.affectedRows === 1) {
-              if (err) console.error(err);
-              else {
-                db.query(newCommande, (err,result) => {
-                  if (err) console.error(err);
-                  else res.status(200).json(result);
-                });
-              }
-            } else res.status(400).send('Erreur l\'id : "'+id+'" n\'existe pas');
-          })
-        } else res.status(400).send('La date et l\'heure de la livraison ne sont pas au bont format : YYYY-MM-DD HH:MM:SS');
-      } else res.status(400).send('L\'email n\'est pas au bont format');
+    if (typeof email !== 'undefined' && typeof name !== 'undefined' && typeof livraison !== 'undefined') {
+        if (email.match(patternMail)) {
+            if (livraison.match(patternLivraison)) {
+                let query = `UPDATE commande SET nom = '${name}', mail = '${email}', updated_at = '${dateActu}', livraison = '${livraison}' where id = '${id}'`;
+                let newCommande = `Select * from commande where id = '${id}'`;
+                db.query(query, (err, result) => {
+                    if (result.affectedRows === 1) {
+                        if (err) console.error(err);
+                        else {
+                            db.query(newCommande, (err, result) => {
+                                if (err) console.error(err);
+                                else res.status(200).json(result);
+                            });
+                        }
+                    } else res.status(400).send('Erreur l\'id : "' + id + '" n\'existe pas');
+                })
+            } else res.status(400).send('La date et l\'heure de la livraison ne sont pas au bont format : YYYY-MM-DD HH:MM:SS');
+        } else res.status(400).send('L\'email n\'est pas au bont format');
     } else res.status(400).send('Veuillez entrer un nom, un mail et la date et l\'heure de livraison (nom, mail, livraison)');
 });
 
 // Les autres méthodes ne sont pas allowed
 
 app.put("/*", (req, res) => {
-  let erreur = {
-    "type": "error",
-    "error": 405,
-    "message": "Method not allowed"
-  }
-  JSON.stringify(erreur)
-  res.send(erreur)
+    let erreur = {
+        "type": "error",
+        "error": 405,
+        "message": "Method not allowed"
+    }
+    JSON.stringify(erreur)
+    res.send(erreur)
 });
 
 app.delete("/*", (req, res) => {
-  let erreur = {
-    "type": "error",
-    "error": 405,
-    "message": "Method not allowed"
-  }
-  JSON.stringify(erreur)
-  res.send(erreur)
+    let erreur = {
+        "type": "error",
+        "error": 405,
+        "message": "Method not allowed"
+    }
+    JSON.stringify(erreur)
+    res.send(erreur)
 });
 
 app.listen(PORT, HOST);
 console.log(`Commande API Running on http://${HOST}:${PORT}`);
 
 const db = mysql.createConnection({
-  host: "mysql.commande",
-  user: "command_lbs",
-  password: "command_lbs",
-  database: "command_lbs"
+    host: "mysql.commande",
+    user: "command_lbs",
+    password: "command_lbs",
+    database: "command_lbs"
 });
 
 db.connect(err => {
-  if (err) {
-    console.error(err);
-  }
-  console.log("Connected to database");
+    if (err) {
+        console.error(err);
+    }
+    console.log("Connected to database");
 });
 
 // Pour y accéder, port 19080
